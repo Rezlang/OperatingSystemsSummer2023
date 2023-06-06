8cf60fae91ae6399a772066b685d80e70acedc9c
Rezlang <96161550+Rezlang@users.noreply.github.com>
2023-06-06 10:52:21 -0400

    final comments

--- Homeworks/Homework 1/hw1.c
+++ Homeworks/Homework 1/hw1.c
@@ -4,6 +4,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+// Takes input of a string and adds the ascii values of the characters
+// returns that value modulo the size of the cache.
 int hash(char* string, int cacheSize) {
   int hash = 0;
   int i = 0;
@@ -14,6 +16,7 @@ int hash(char* string, int cacheSize) {
   return hash % cacheSize;
 }
 
+// Determines whether a string is a valid word
 bool isWord(const char* word) {
   if (strlen(word) < 2) return false;
   for (int i = 0; *(word + i) != '\0'; i++) {
@@ -24,19 +27,19 @@ bool isWord(const char* word) {
   return true;
 }
 
-// Word "up" ==> 8 [realloc]
-// Word "and" ==> 1 [calloc]
-// Word "down" ==> 15 [nop]
+// Adds a word to the correct place in the cache
 int addWord(char** cache, char* word, int cacheSize) {
+  // default is index is the correct size
   char* allocType = "nop";
   if (!isWord(word)) return 0;
   int index = hash(word, cacheSize);
   if (*(cache + index) == NULL) {
+    // if index is empty
     *(cache + index) = (char*)calloc(strlen(word) + 1, sizeof(char));
     allocType = "calloc";
   } else if (strlen(*(cache + index)) != strlen(word)) {
+    // if index is the wrong size
     *(cache + index) = (char*)realloc(*(cache + index), strlen(word) + 2);
-
     allocType = "realloc";
   }
   strcpy(*(cache + index), word);
@@ -44,11 +47,12 @@ int addWord(char** cache, char* word, int cacheSize) {
   return 1;
 }
 
+// takes file input, then reads and processes each word
 int readInFile(char** cache, char* fileName, int cacheSize) {
   FILE* file = fopen(fileName, "r");
   if (file == NULL) {
-    fprintf(stderr, "Failed to open the file.\n");
-    return 0;
+    fprintf(stderr, "ERROR: Failed to open the file.\n");
+    exit(3);
   }
 
   char* word = (char*)calloc(128, sizeof(char));
@@ -74,20 +78,26 @@ int readInFile(char** cache, char* fileName, int cacheSize) {
 
   fclose(file);
   free(word);
-  return 1;
+  return 0;
 }
 
 int main(int argc, char** argv) {
+  // Error checking
   if (argc != 3) {
-    fprintf(stderr, "Incorrect arguments provided\n");
+    fprintf(stderr, "ERROR: Incorrect arguments provided\n");
+    return 1;
   }
   int cacheSize = atoi(*(argv + 1));
   if (cacheSize == 0 && *(*(argv + 1)) != '0') {
-    fprintf(stderr, "Invalid size\n");
+    fprintf(stderr, "ERROR: Invalid size\n");
+    return 2;
   }
+  // File reading
   char* fileName = *(argv + 2);
   char** cache = (char**)calloc(cacheSize, sizeof(char*));
   readInFile(cache, fileName, cacheSize);
+
+  // Final printing of values and freeing of memory
   printf("\nCache:\n");
   for (int i = 0; i < cacheSize; ++i) {
     if (*(cache + i) == NULL) continue;
@@ -95,4 +105,5 @@ int main(int argc, char** argv) {
     free(*(cache + i));
   }
   free(cache);
+  return 0;
 }
